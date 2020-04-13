39071d17d5ff29d353f0a768917917efaa473eb7
Kunal <kunalrai.cse16@nituk.ac.in>
2020-04-14 01:10:02 +0530

    planning

---
+++ cses_1635.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+
+//using namespace __gnu_pbds;
+
+using namespace std;
+
+#define ll  long long
+
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef unordered_map<int,int> um;
+typedef unordered_set<int> us;
+typedef priority_queue<int> pq;
+typedef deque<int> dq;
+typedef queue<int> q;
+typedef stack<int> st;
+
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+
+#define rep(i,a,b) for (int i = a; i < b; i++)
+//#define bitset<n> b<n>
+
+/* typedef tree<int,null_type,less<int>,rb_tree_tag,
+tree_order_statistics_node_update> indexed_set; */
+
+void c_p_c()
+{
+	
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+int main(){
+
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	//indexed_set s;
+	//s.insert(2);
+	//s.insert(3);
+	//s.insert(7);
+	//s.insert(9);
+
+	//auto x = s.find_by_order(2);
+	//s.order_of_key(7)
+	
+	//c_p_c();
+
+	//__builtin_clz(x): the number of zeros at the beginning of the number
+   // __builtin_ctz(x): the number of zeros at the end of the number
+   // __builtin_popcount(x): the number of ones in the number
+   // __builtin_parity(x): the parity (even or odd) of the number of ones
+
+	ll n,x,val;
+	cin>>n>>x;
+	
+	vector<ll> v;
+	
+	rep(i,0,n){
+		cin>>val;
+		v.pb(val);
+	}
+	
+	vector<ll> count(x+1,0);
+	count[0]=1;
+
+	for(ll i=1;i<=x;i++){
+		for(ll ele: v){
+			if((i-ele)>=0)
+				count[i] += count[i-ele]%1000000007;
+		}
+	}
+
+	cout<<count[x]%1000000007;
+
+	return 0;
+	}
+
+		
